4e09c947b07659e4b798baa32f908077cbc9a84d
HE BIN <32996932+dlhb01@users.noreply.github.com>
2018-12-17 10:20:34 +0800

    Update world.py

--- world.py
+++ world.py
@@ -1,11 +1,16 @@
 import requests
 from lxml import html
 
-USERNAME = "<USERNAME>"
-PASSWORD = "<PASSWORD>"
+f=open("../security/credentials.txt","r")
+lines=f.readlines()
+username=lines[0]
+username=username[0:len(username)-1] #remove the enter char
+password=lines[1]
+f.close()
 
-LOGIN_URL = "http://www.bbdc.sg/bbweb/default.aspx"
-URL = "http://www.bbdc.sg/bbdc/b-selectCourse.asp"
+
+LOGIN_URL = "https://nussweb.org.sg/nussweb/main/loginuser.asp"
+URL = "https://nussweb.org.sg/nussweb/main/main.asp"
 
 def main():
 
@@ -14,20 +19,21 @@ def main():
     headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     #print (headers)
     # Get login csrf token
-    result = session_requests.get(LOGIN_URL,headers=headers)
-    tree = html.fromstring(result.text)
+    #result = session_requests.get(LOGIN_URL,headers=headers)
+    #tree = html.fromstring(result.text)
     #print(result.text)
     #authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
 
     # Create payload
     payload = {
-        "txtNRIC": USERNAME, 
-        "txtPassword": PASSWORD, 
+        "txtMemberID": username, 
+        "txtPassword": password, 
      #   "csrfmiddlewaretoken": authenticity_token
     }
 
     # Perform login
     result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
+    print(result.text)
 
     # Scrape url
     result = session_requests.get(URL, headers = dict(referer = URL))
